09065b6ab538e26d8608d9a3ed3b6a2e1078e5ed
Chandrahaas Jasti <chandrahaasjasti@gmail.com>
2026-04-02 20:59:32 +0530

    override the chatservice, and mocked the response

--- AIService/services/chat.py
+++ AIService/services/chat.py
@@ -1,4 +1,5 @@
 from all_in_one.LLMservice import GeminiClient
+from typing_extensions import override
 class AIChatService:
     def __init__(self,llm_client=None):
         if (llm_client==None):
@@ -41,3 +42,26 @@ class AIChatService:
             raise Exception(str(e)) from e
         except Exception as e:
             raise Exception(f"Lesson chat failed: {e}") from e
+
+class AIChatServiceV2(AIChatService):
+    def __init__(self,llm_client=None):
+        super().__init__(llm_client)
+
+    @override
+    def chat(self, question: str) -> str:
+        resp="""
+        {
+            "AI_Response": "Hello, how can I help you today?",
+            "is_ready": true,
+            "component_name": "roadmap",
+            "metadata": {
+                "title": "My Dashboard",
+                "content": "Sample text",
+                "actions":{
+                    "build_roadmap": true,
+                    "build_book": true
+                }
+            }
+        }
+        """
+        return resp
